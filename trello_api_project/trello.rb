9f971eac6a62d78b77c71e66e619e082f502f7d2
Mateusz Buczek <mtsbck@gmail.com>
2022-07-18 20:20:56 +0200

    creation,deletion and the likes

--- trello_api_project/trello.rb
+++ trello_api_project/trello.rb
@@ -7,6 +7,8 @@ class TrelloApiCommunicator
     config = JSON.parse(FileWork.new(folder: '.').read_file('config.json'))
     @key = config['api_key']
     @token = config['api_token']
+    @auth = "key=#{@key}&token=#{@token}"
+    @address = "https://api.trello.com/1"
   end
 
   def ddos_google
@@ -14,7 +16,32 @@ class TrelloApiCommunicator
   end
 
   def get_board(board: 'QQuJXFB7')
-    Faraday.get("https://api.trello.com/1/boards/#{board}/memberships?key=#{@key}&token=#{@token}")
+    Faraday.get("#{@address}/boards/#{board}/memberships?#{@auth}")
+  end
+
+  def pull_lists_from_board(board: 'QQuJXFB7')
+    Faraday.get("#{@address}/boards/#{board}/lists?#{@auth}")
+  end
+
+  def pull_cards_from_list(list: '62b0a89f9e8ba22e91a1d6be')
+    Faraday.get("#{@address}/lists/#{list}/cards?#{@auth}")
+  end
+
+  def add_card_to_a_list(list_id: '62b0a89f9e8ba22e91a1d6be', card_name: 'Testowa etykieta rubiego hue')
+    Faraday.post("#{@address}/cards?#{@auth}&idList=#{list_id}&name=#{card_name}")
+  end
+
+  def delete_card_from_a_list(card_name)
+    Faraday.delete("#{@address}/cards/#{card_name}?#{@auth}")
+  end
+
+  def delete_all_cards_from_list(list: '62b0a89f9e8ba22e91a1d6be')
+    list_of_cards = JSON.parse(pull_cards_from_list(list: list).body)
+    size_of_list = list_of_cards.length
+    size_of_list.times do |x|
+      first_card_id = list_of_cards[x]['id']
+      delete_card_from_a_list(first_card_id)
+    end
   end
 end
 
--- trello_api_project/trello_test.rb
+++ trello_api_project/trello_test.rb
@@ -26,16 +26,58 @@ class TrelloTest < Minitest::Test
     assert_equal 200, ddos_google.ddos_google
   end
 
+  def test_successfully_querying_trello
+    # skip
+    board = TrelloApiCommunicator.new.get_board
+    assert_equal 200, board.status
+  end
+
   def test_getting_zielony_board
     # skip
     board = TrelloApiCommunicator.new.get_board
     assert_equal 'admin', JSON.parse(board.body).first['memberType']
   end
 
-  def test_successfully_querying_trello
+  def test_getting_lists_from_board
     # skip
-    board = TrelloApiCommunicator.new.get_board
-    assert_equal 200, board.status
+    lists_from_board = TrelloApiCommunicator.new.pull_lists_from_board()
+    board_size = JSON.parse(lists_from_board.body).length
+    assert_equal 200, lists_from_board.status
+    assert_equal 4, board_size
+  end
+
+  def test_getting_cards_from_list
+    # skip
+    list_of_cards = TrelloApiCommunicator.new.pull_cards_from_list()
+    assert_equal 200, list_of_cards.status
+  end
+
+  def test_creating_a_card_on_a_list
+    # skip
+    new_card = TrelloApiCommunicator.new.add_card_to_a_list()
+    assert_equal 200, new_card.status
+  end
+
+  def test_delete_the_first_card_from_a_list
+    # skip
+    TrelloApiCommunicator.new.add_card_to_a_list()
+    list_of_cards = JSON.parse(TrelloApiCommunicator.new.pull_cards_from_list.body)
+    size_of_list = list_of_cards.length
+    first_card_id = list_of_cards[0]['id']
+    deletion = TrelloApiCommunicator.new.delete_card_from_a_list(first_card_id)
+    size_of_list_after_deletion = JSON.parse(TrelloApiCommunicator.new.pull_cards_from_list.body).length
+    assert_equal 200, deletion.status
+    assert_equal size_of_list - 1, size_of_list_after_deletion
+  end
+
+  def test_delete_all_cards_on_a_list
+    # skip
+    5.times do
+      TrelloApiCommunicator.new.add_card_to_a_list()
+    end
+    TrelloApiCommunicator.new.delete_all_cards_from_list
+    size_of_list_after_deletion = JSON.parse(TrelloApiCommunicator.new.pull_cards_from_list.body).length
+    assert_equal 0, size_of_list_after_deletion
   end
 
 end
